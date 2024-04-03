8009adf7782162410bd0232a3385af63f6e7f6b9
Scott Benton <scott@scottbenton.dev>
2024-04-03 02:32:47 +0000

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import tsConfigPaths from "vite-tsconfig-paths";
 import {VitePWA} from "vite-plugin-pwa"
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr(), tsConfigPaths(), VitePWA({registerType:"autoUpdate"})],
+export default ({mode}) => { 
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+  plugins: [react(), svgr(), tsConfigPaths(), VitePWA({registerType:"autoUpdate", includeAssets: [process.env.VITE_FAVICON_PATH],
+      manifest: {
+        name: process.env.VITE_TITLE,
+        short_name: process.env.VITE_TITLE,
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: process.env.VITE_FAVICON_PATH,
+            sizes: '192x192',
+            type: 'image/svg'
+          },
+          {
+            src: process.env.VITE_FAVICON_PATH,
+            sizes: '512x512',
+            type: 'image/svg'
+          }
+        ]
+      }})],
   optimizeDeps: {
     // exclude: [
     //   "firebase",
@@ -15,4 +36,5 @@ export default defineConfig({
     //   "firebase/analytics",
     // ],
   },
-});
+})
+};
