ac2bd97b93c3ab2a8b4b145d7987017b8c23e6be
Scott Benton <scott@scottbenton.dev>
2024-05-29 23:11:46 -0400

    fix(homebrew): Fixed homebrew not being removed from characters when deleted

--- src/api-calls/homebrew/deleteHomebrewExpansion.ts
+++ src/api-calls/homebrew/deleteHomebrewExpansion.ts
@@ -1,22 +1,56 @@
-import { deleteDoc } from "firebase/firestore";
+import {
+  arrayRemove,
+  deleteDoc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { getHomebrewCollectionDoc } from "./_getRef";
 import { createApiFunction } from "api-calls/createApiFunction";
+import { getCharacterCollection } from "api-calls/character/_getRef";
+import { getCampaignCollection } from "api-calls/campaign/_getRef";
 
 export const deleteHomebrewExpansion = createApiFunction<{ id: string }, void>(
-  (params) => {
+  async (params) => {
     const { id } = params;
 
-    return new Promise((resolve, reject) => {
-      const promises: Promise<unknown>[] = [];
+    const characterLoadPromise = getDocs(
+      query(
+        getCharacterCollection(),
+        where("expansionIds", "array-contains", id)
+      )
+    );
 
-      promises.push(deleteDoc(getHomebrewCollectionDoc(id)));
+    const campaignLoadPromise = getDocs(
+      query(
+        getCampaignCollection(),
+        where("expansionIds", "array-contains", id)
+      )
+    );
 
-      Promise.all(promises)
-        .then(() => {
-          resolve();
+    const characterSnapshot = await characterLoadPromise;
+    const campaignSnapshot = await campaignLoadPromise;
+
+    const promises: Promise<unknown>[] = [];
+
+    characterSnapshot.docs.forEach((doc) => {
+      promises.push(
+        updateDoc(doc.ref, {
+          expansionIds: arrayRemove(id),
         })
-        .catch(reject);
+      );
     });
+    campaignSnapshot.docs.forEach((doc) => {
+      promises.push(
+        updateDoc(doc.ref, {
+          expansionIds: arrayRemove(id),
+        })
+      );
+    });
+
+    await Promise.all(promises);
+    deleteDoc(getHomebrewCollectionDoc(id));
   },
   "Failed to delete expansion."
 );
