5f71109fda446965e420a2d2b763754967c8670f
Scott Benton <scott@scottbenton.dev>
2024-06-14 00:38:07 -0400

    fix(locations): Fixed location card secondary text

--- src/hooks/featureFlags/activeFeatureFlags.ts
+++ src/hooks/featureFlags/activeFeatureFlags.ts
@@ -4,10 +4,4 @@ export const activeFeatureFlags: {
   testId: string;
   label: string;
   gameSystems?: GAME_SYSTEMS[];
-}[] = [
-  {
-    testId: "maps-update",
-    label:
-      "Maps Update - location improvements, and eventual addition of maps to Iron Fellowship",
-  },
-];
+}[] = [];
