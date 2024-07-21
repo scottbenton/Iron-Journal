08bb0f88d35bb77faf76dd3aef3404350993f7ed
Scott <scott@scottbenton.dev>
2024-07-20 21:01:01 -0400

    fix(a11y): Added accessible labels to legacy tracks

--- src/pages/Character/CharacterSheetPage/Tabs/TracksSection/LegacyTrack.tsx
+++ src/pages/Character/CharacterSheetPage/Tabs/TracksSection/LegacyTrack.tsx
@@ -12,6 +12,8 @@ import UncheckedIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckedIcon from "@mui/icons-material/CheckBox";
 import MinusIcon from "@mui/icons-material/Remove";
 import PlusIcon from "@mui/icons-material/Add";
+import { useId } from "react";
+import { useStore } from "stores/store";
 
 export interface LegacyTrackProps {
   label: string;
@@ -53,6 +55,13 @@ export function LegacyTrack(props: LegacyTrackProps) {
     }
   }
 
+  const labelId = useId();
+  const getValueText = (value: number) => {
+    return `${value} ticks: (${Math.floor(value / 4)} boxes fully filled)`;
+  };
+
+  const announce = useStore((store) => store.appState.announce);
+
   return (
     <Box display={"flex"}>
       <Box>
@@ -66,6 +75,7 @@ export function LegacyTrack(props: LegacyTrackProps) {
             color={(theme) => theme.palette.text.primary}
             fontFamily={(theme) => theme.fontFamilyTitle}
             sx={{ mr: 4 }}
+            id={labelId}
           >
             {label}
           </Typography>
@@ -92,7 +102,17 @@ export function LegacyTrack(props: LegacyTrackProps) {
         >
           {onValueChange && (
             <ButtonBase
-              onClick={() => onValueChange(value > 0 ? value - 1 : 0)}
+              onClick={() => {
+                if (onValueChange) {
+                  const newValue = Math.max(value - 1, 0);
+                  onValueChange(newValue);
+                  if (newValue === value) {
+                    announce(`${label} is already at zero ticks`);
+                  } else {
+                    announce(`Updated ${label} to ${getValueText(newValue)}`);
+                  }
+                }
+              }}
               sx={(theme) => ({
                 height: isMobile ? 34 : 48,
                 backgroundColor:
@@ -111,70 +131,102 @@ export function LegacyTrack(props: LegacyTrackProps) {
               <MinusIcon />
             </ButtonBase>
           )}
-          {checks.map((value, index, arr) => (
-            <Box key={index}>
-              <Box
-                sx={(theme) => ({
-                  borderStyle: "solid",
-                  borderColor: theme.palette.divider,
-                  borderLeftColor:
-                    index !== 0 || !onValueChange
-                      ? theme.palette.divider
-                      : "transparent",
-                  borderRightColor:
-                    index === arr.length - 1 && !onValueChange
-                      ? theme.palette.divider
-                      : "transparent",
-                  borderWidth: 1,
-                  width: isMobile ? 34 : 48,
-                  height: isMobile ? 34 : 48,
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                })}
-              >
-                <ProgressTrackTick
-                  value={value}
+          <Box>
+            <Box
+              display={"flex"}
+              bgcolor={(theme) => theme.palette.background.paper}
+              color={(theme) =>
+                theme.palette.mode === "light"
+                  ? theme.palette.grey[600]
+                  : theme.palette.grey[300]
+              }
+              borderTop={1}
+              borderBottom={1}
+              borderLeft={onValueChange ? 0 : 1}
+              borderRight={onValueChange ? 0 : 1}
+              borderColor={(theme) => theme.palette.divider}
+              role={"meter"}
+              aria-labelledby={labelId}
+              aria-valuemin={0}
+              aria-valuemax={40}
+              aria-valuenow={value}
+              aria-valuetext={getValueText(value)}
+            >
+              {checks.map((value, index) => (
+                <Box
                   key={index}
-                  size={{ mobile: 32, desktop: 40 }}
-                />
-              </Box>
-              <Box
-                display={"flex"}
-                alignItems={"center"}
-                justifyContent={"center"}
-                fontSize={isMobile ? 16 : 24}
-              >
-                <Checkbox
-                  sx={{ p: 0 }}
-                  icon={<UncheckedIcon fontSize={"inherit"} />}
-                  checkedIcon={<CheckedIcon fontSize={"inherit"} />}
-                  disabled={value !== 4 || !onExperienceChecked}
-                  checked={checkedExperience[index * 2] ?? false}
-                  onChange={(evt, checked) =>
-                    onExperienceChecked &&
-                    onExperienceChecked(index * 2, checked)
-                  }
-                />
-                {!isLegacy && (
+                  sx={(theme) => ({
+                    borderWidth: 1,
+                    borderStyle: "solid",
+                    borderColor: "transparent",
+                    borderLeftColor:
+                      index !== 0 ? theme.palette.divider : undefined,
+                    width: isMobile ? 34 : 48,
+                    height: isMobile ? 34 : 48,
+                  })}
+                >
+                  <ProgressTrackTick
+                    value={value}
+                    key={index}
+                    aria-hidden
+                    size={{ mobile: 32, desktop: 40 }}
+                  />
+                </Box>
+              ))}
+            </Box>
+            <Box display={"flex"}>
+              {checks.map((value, index) => (
+                <Box
+                  display={"flex"}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  fontSize={isMobile ? 16 : 24}
+                  key={index}
+                  width={isMobile ? 34 : 48}
+                >
                   <Checkbox
                     sx={{ p: 0 }}
                     icon={<UncheckedIcon fontSize={"inherit"} />}
                     checkedIcon={<CheckedIcon fontSize={"inherit"} />}
                     disabled={value !== 4 || !onExperienceChecked}
-                    checked={checkedExperience[index * 2 + 1] ?? false}
+                    checked={checkedExperience[index * 2] ?? false}
                     onChange={(evt, checked) =>
                       onExperienceChecked &&
-                      onExperienceChecked(index * 2 + 1, checked)
+                      onExperienceChecked(index * 2, checked)
                     }
                   />
-                )}
-              </Box>
+                  {!isLegacy && (
+                    <Checkbox
+                      sx={{ p: 0 }}
+                      icon={<UncheckedIcon fontSize={"inherit"} />}
+                      checkedIcon={<CheckedIcon fontSize={"inherit"} />}
+                      disabled={value !== 4 || !onExperienceChecked}
+                      checked={checkedExperience[index * 2 + 1] ?? false}
+                      onChange={(evt, checked) =>
+                        onExperienceChecked &&
+                        onExperienceChecked(index * 2 + 1, checked)
+                      }
+                    />
+                  )}
+                </Box>
+              ))}
             </Box>
-          ))}
+          </Box>
           {onValueChange && (
             <ButtonBase
-              onClick={() => onValueChange(value < 40 ? value + 1 : 40)}
+              onClick={() => {
+                if (onValueChange) {
+                  const newValue = Math.min(value + 1, 40);
+                  onValueChange(newValue);
+                  if (newValue === value) {
+                    announce(
+                      `${label} is already at its maximum value of 40 ticks`
+                    );
+                  } else {
+                    announce(`Updated ${label} to ${getValueText(newValue)}`);
+                  }
+                }
+              }}
               sx={(theme) => ({
                 height: isMobile ? 34 : 48,
                 backgroundColor:
